0c400a874afb7a9261576628d716c88f23bedf64
damien viel <damienviel69@gmail.com>
2024-04-14 19:37:26 -0400

    Rails simple rbnb done with research fonctionality

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,7 +2,11 @@ class FlatsController < ApplicationController
   before_action :set_flat, only: [:show, :edit, :update, :destroy]
 
   def index
-    @flats = Flat.all
+    if params[:name].present?
+      @flats = Flat.where("name LIKE ?", "%#{params[:name]}%")
+    else
+      @flats = Flat.all
+    end
   end
 
   def show
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "../views/flats/index.html.erb"
+
+document.querySelector("search-input").addEventListener('click', event => {
+  event.preventDefault();
+})
